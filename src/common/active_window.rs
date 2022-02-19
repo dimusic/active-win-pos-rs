1de0f591a9a1f2f81479849879430f8f0db2e73a
Dmitry Malkov <targtn@gmail.com>
2022-02-18 21:01:58 -0600

    Implement PlatformApi::get_active_window for Windows

--- src/common/active_window.rs
+++ src/common/active_window.rs
@@ -3,6 +3,6 @@ use super::window_position::WindowPosition;
 #[derive(Debug)]
 pub struct ActiveWindow {
     pub window_id: String,
-    pub process_id: i64,
+    pub process_id: u64,
     pub position: WindowPosition
 }
--- src/win/platform_api.rs
+++ src/win/platform_api.rs
@@ -1,10 +1,11 @@
-use crate::common::platform_api::PlatformApi;
+use crate::{common::platform_api::PlatformApi, ActiveWindow};
 use crate::common::window_position::WindowPosition;
+use winapi::shared::windef::HWND__;
+use winapi::um::winuser::{GetWindowThreadProcessId};
 use winapi::{
     shared::windef::{ RECT },
     um::{winuser::{ GetForegroundWindow, GetWindowRect }}
 };
-use std::{io::Error};
 use super::window_position::FromWinRect;
 
 pub struct WindowsPlatformApi {
@@ -13,32 +14,53 @@ pub struct WindowsPlatformApi {
 
 impl PlatformApi for WindowsPlatformApi {
     fn get_position(&self) -> Result<WindowPosition, ()> {
-        if let Ok(win_position) = get_foreground_window_position() {
+        let active_window = get_foreground_window()?;
+
+        if let Ok(win_position) = get_foreground_window_position(active_window) {
             return Ok(WindowPosition::from_win_rect(&win_position));
         }
 
         Ok(WindowPosition::new(0 as f64, 0 as f64, 0 as f64, 0 as f64))
     }
+
+    fn get_active_window(&self) -> Result<ActiveWindow, ()> {
+        let active_window = get_foreground_window()?;
+        let active_window_position = self.get_position()?;
+
+        let lpdw_process_id = unsafe {
+            let pid_ptr: *mut u32 = std::mem::zeroed();
+            GetWindowThreadProcessId(active_window, pid_ptr)
+        };
+
+        let active_window = ActiveWindow {
+            position: active_window_position,
+            process_id: lpdw_process_id as u64,
+            window_id: format!("{:?}", active_window),
+        };
+
+        Ok(active_window)
+    }
 }
 
-fn get_foreground_window_position() -> Result<RECT, Error> {
-    let active_window = unsafe {
-        GetForegroundWindow()
-    };
+fn get_foreground_window() -> Result<*mut HWND__, ()> {
+    let active_window = unsafe { GetForegroundWindow() };
 
     if active_window.is_null() {
-        return Err(Error::last_os_error());
+        return Err(());
     }
-    else {
-        unsafe {
-            let mut rect: RECT = std::mem::zeroed();
 
-            if GetWindowRect( active_window, &mut rect as *mut RECT) == 1 {
-                return Ok(rect);
-            }
-            else {
-                return Err(Error::last_os_error());
-            }
-        };
-    }
+    Ok(active_window)
+}
+
+fn get_foreground_window_position(hwnd: *mut HWND__) -> Result<RECT, ()> {
+    unsafe {
+        let mut rect: RECT = std::mem::zeroed();
+
+        if GetWindowRect( hwnd, &mut rect as *mut RECT) == 1 {
+            return Ok(rect);
+        }
+        else {
+            return Err(());
+        }
+    };
 }
