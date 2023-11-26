205bd19ca762a7ad059fc2a6579bb00503d5b41a
Germano Eichenberg <germanoeich@gmail.com>
2023-11-26 02:55:29 -0300

    Fix string conversions

--- examples/active-window.rs
+++ examples/active-window.rs
@@ -3,7 +3,7 @@ use active_win_pos_rs::get_active_window;
 fn main() {
     match get_active_window() {
         Ok(active_window) => {
-            println!("active window: {:#?}", active_window);
+            println!("active window: {}", active_window.title);
         }
         Err(()) => {
             println!("error occurred while getting the active window");
